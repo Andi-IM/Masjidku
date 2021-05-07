3e7bd4e61b38b59f55713f3c6fe9f21490481d73
Andi-IM <21165698+Andi-IM@users.noreply.github.com>
2021-05-07 12:19:38 +0700

    Provide user privilege - Admin - Principal - Secretary - Accountant

---
+++ src/main/java/org/masjidku/admin/HomeController.java
@@ -0,0 +1,13 @@
+package org.masjidku.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+
+    @FXML
+    public void handleLoginPage() {
+
+    }
+}
