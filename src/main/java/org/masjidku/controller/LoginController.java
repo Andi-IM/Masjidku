ab529a9350e0665616b99ea5ae7d1e2f1c7d655c
Andi-IM <21165698+Andi-IM@users.noreply.github.com>
2021-05-08 10:37:39 +0700

    Simplify Project structure, adding login method

--- src/main/java/org/masjidku/controller/LoginController.java
+++ src/main/java/org/masjidku/controller/LoginController.java
@@ -1,6 +1,9 @@
 package org.masjidku.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.masjidku.MainApp;
+
 
 public class LoginController {
     // Reference to the main application
@@ -11,4 +14,22 @@ public class LoginController {
      * @param mainApp the context
      */
     public void setMainApp(MainApp mainApp){ this.mainApp = mainApp; }
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    private boolean buttonPressed = false;
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    public void initialize(){ }
+
+    @FXML
+    public void handleLogin(){
+        String un = username.getText();
+        String ps = password.getText();
+    }
 }
