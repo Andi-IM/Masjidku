76a006b835dee3344d5f6e60fb8ccc906be55b69
luthfiyyahnisaa <luthfiyyahnisaa@gmail.com>
2021-05-24 16:06:02 +0700

    membuat secretary home, root, kelola kegiatan

---
+++ src/main/java/org/masjidku/secretary/SecretaryKegiatanForm.java
@@ -0,0 +1,18 @@
+package org.masjidku.secretary;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class SecretaryKegiatanForm {
+    public void clearForm(ActionEvent actionEvent) {
+    }
+
+    public void onUserSubmitted(ActionEvent actionEvent) {
+    }
+
+    public void gotoList(ActionEvent actionEvent) {
+    }
+
+    public void onLogoutClick(MouseEvent mouseEvent) {
+    }
+}
