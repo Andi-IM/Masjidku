3e5a077728ca0d9d4b9c18b576874d80b467b316
Andi Irham <Andi.irhamm@gmail.com>
2021-06-17 21:35:39 +0700

    anak yatim accountant business rules

--- src/main/java/org/masjidku/accountant/AccountantAnakyatim.java
+++ src/main/java/org/masjidku/accountant/AccountantAnakyatim.java
@@ -15,33 +15,39 @@
 
 package org.masjidku.accountant;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import org.masjidku.MainApp;
+import org.masjidku.model.accounting.DaoFunctions;
+import org.masjidku.model.accounting.anakyatim.AnakYatim;
 import org.masjidku.model.accounting.anakyatim.AnakYatimDao;
+import org.masjidku.model.accounting.anakyatim.DonasiAYatim;
 import org.masjidku.model.accounting.anakyatim.DonasiAYatimDao;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class AccountantAnakyatim implements Initializable {
-    @FXML
-    public Button btnDaftarAnakYatim;
-    @FXML
-    public Button btnDaftarDonasiAnakYatim;
+
     @FXML
     public Text txtPemasukanTerakhir;
     @FXML
     public Text txtPengeluaranTerakhir;
     @FXML
-    public Text txtTerakhirDiubah;
+    public Text txtTglPemasukkan;
+    @FXML
+    public Text txtTotalPemasukkan;
+    @FXML
+    public Text txtTotalPengeluaran;
+    @FXML
+    public Text txtSaldo;
+    @FXML
+    public Text txtTglPengeluaran;
 
     private MainApp mainApp;
+
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
@@ -50,16 +56,34 @@ public class AccountantAnakyatim implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         AnakYatimDao ayDao = new AnakYatimDao();
         DonasiAYatimDao dayDao = new DonasiAYatimDao();
+        DaoFunctions df = new DaoFunctions();
 
+        try {
+            if (ayDao.getConnection() && dayDao.getConnection() && df.getConnection()) {
+                AnakYatim penerima = ayDao.getLastRecord();
+                DonasiAYatim pemberi = dayDao.getLastRecord();
+
+                txtPemasukanTerakhir.setText("Rp. " + penerima.getJumlah());
+                txtPengeluaranTerakhir.setText("Rp. " + pemberi.getJumlah());
+                txtTotalPemasukkan.setText("Rp. " + dayDao.getTotalIncome());
+                txtTotalPengeluaran.setText("Rp. " + ayDao.getTotalOutcome());
+                txtSaldo.setText("Rp. " + df.getInfakYatimBalance());
+                txtTglPemasukkan.setText(pemberi.getTanggal());
+                txtTglPengeluaran.setText(penerima.getTanggal());
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void onLogoutClick(MouseEvent mouseEvent) {
-    }
+    @FXML
+    public void onLogoutClick() { mainApp.onLogoutAction(); }
 
-    public void daftarDonasiAyatim(ActionEvent actionEvent) {
-    }
+    @FXML
+    public void onKelolaDonasiAYatim() {  mainApp.showDonasiAYatim(); }
 
-    public void daftarAnakYatim(ActionEvent actionEvent) {
-    }
+    @FXML
+    public void onKelolaDanaAYatim() { mainApp.showDaftarAnakYatim(); }
 }
--- src/main/java/org/masjidku/model/accounting/anakyatim/DonasiAYatimDao.java
+++ src/main/java/org/masjidku/model/accounting/anakyatim/DonasiAYatimDao.java
@@ -18,7 +18,6 @@ package org.masjidku.model.accounting.anakyatim;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.masjidku.model.Dao;
-import org.masjidku.model.DaoFactory;
 
 import java.sql.SQLException;
 
@@ -99,6 +98,35 @@ public class DonasiAYatimDao extends Dao<DonasiAYatim> {
         ps = con.prepareStatement(query);
         ps.setString(1, id);
         ps.executeUpdate();
+    }
+
+    public DonasiAYatim getLastRecord() throws SQLException {
+        query = "SELECT * FROM "+TABLE+" ORDER BY ID DESC LIMIT 1";
+        ps = con.prepareStatement(query);
+        rs = ps.executeQuery();
+
+        DonasiAYatim model = null;
+        if (rs.next()){
+            model = new DonasiAYatim(
+                    rs.getString(1),
+                    rs.getString(2),
+                    Double.parseDouble(rs.getString(3)),
+                    rs.getString(4),
+                    rs.getString(5)
+            );
+        }
+        return model;
+    }
+
+    public String getTotalIncome() throws SQLException {
+        query = "SELECT SUM(jumlah) FROM "+TABLE;
+        ps = con.prepareStatement(query);
+        rs = ps.executeQuery();
+
+        if (rs.next()){
+            return rs.getString(1);
+        }
+        return null;
     }
 
     public boolean isDonaturExist(String id) throws SQLException {
