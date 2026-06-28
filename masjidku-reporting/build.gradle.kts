99b8b404654c289ad5d563f02f58d6bfd726fa6e
Andi-IM <21165698+Andi-IM@users.noreply.github.com>
2026-06-28 22:20:15 +0700

    fix: resolve jasperreports module compilation errors in reporting module

--- masjidku-reporting/build.gradle.kts
+++ masjidku-reporting/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation(libs.openpdf)
     implementation(libs.jasperreports) {
         exclude(group = "com.lowagie", module = "itext")
+        exclude(group = "xml-apis", module = "xml-apis")
+        exclude(group = "xml-apis", module = "xml-apis-ext")
     }
     implementation("org.slf4j:slf4j-api:2.0.12")
 }
--- masjidku-reporting/src/main/java/module-info.java
+++ masjidku-reporting/src/main/java/module-info.java
@@ -2,8 +2,9 @@ module org.masjidku.reporting {
     requires transitive org.masjidku.reporting.client;
     requires java.sql;
     requires org.masjidku.common;
-    requires jasperreports;
+    requires net.sf.jasperreports.core;
     requires org.slf4j;
+    requires java.desktop;
 
     provides org.masjidku.reporting.client.service.ReportService 
         with org.masjidku.reporting.service.impl.ReportServiceImpl;
