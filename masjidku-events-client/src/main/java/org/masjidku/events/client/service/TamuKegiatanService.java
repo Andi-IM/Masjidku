12c3d1fc98b08a33c8c82fe31081a806d8fc2d3c
Andi-IM <21165698+Andi-IM@users.noreply.github.com>
2026-06-28 18:51:56 +0700

    chore: code-cleanup

--- masjidku-events-client/src/main/java/org/masjidku/events/client/service/TamuKegiatanService.java
+++ masjidku-events-client/src/main/java/org/masjidku/events/client/service/TamuKegiatanService.java
@@ -5,10 +5,16 @@ import org.masjidku.events.client.model.TamuKegiatan;
 
 public interface TamuKegiatanService {
     TamuKegiatan get(String id) throws java.sql.SQLException;
+
     ObservableList<TamuKegiatan> getAll() throws java.sql.SQLException;
+
     void save(TamuKegiatan model) throws java.sql.SQLException;
+
     void save(String idKegiatan, String idTamu, String keterangan, String opeartor) throws java.sql.SQLException;
+
     void update(String[] params) throws java.sql.SQLException;
+
     void delete(String id) throws java.sql.SQLException;
+
     boolean isUndanganExist(String id) throws java.sql.SQLException;
 }
--- masjidku-events-client/src/main/java/org/masjidku/events/client/service/TamuService.java
+++ masjidku-events-client/src/main/java/org/masjidku/events/client/service/TamuService.java
@@ -5,11 +5,18 @@ import org.masjidku.events.client.model.Tamu;
 
 public interface TamuService {
     Tamu get(String id) throws java.sql.SQLException;
+
     ObservableList<Tamu> getAll() throws java.sql.SQLException;
+
     void save(Tamu model) throws java.sql.SQLException;
+
     void update(String[] params) throws java.sql.SQLException;
+
     void delete(String id) throws java.sql.SQLException;
+
     boolean isTamuExist(String id) throws java.sql.SQLException;
+
     ObservableList<String> getAllTamuName() throws java.sql.SQLException;
+
     String getIdByName(String name) throws java.sql.SQLException;
 }
