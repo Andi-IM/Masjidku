bef3e93919ba9d7db832c126c8de6a10b9b42d20
Andi-IM <Andi.irhamm@users.noreply.github.com>
2026-07-02 08:49:41 +0700

    Remove incorrect @AttributeOverride in TpaMasukEntity

--- masjidku-accounting/src/main/java/org/masjidku/accounting/domain/entity/TpaMasukEntity.java
+++ masjidku-accounting/src/main/java/org/masjidku/accounting/domain/entity/TpaMasukEntity.java
@@ -1,12 +1,8 @@
 package org.masjidku.accounting.domain.entity;
 
-import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "infak_tpa")
-@AttributeOverride(name = "donatur", column = @Column(name = "nama"))
-public class TpaMasukEntity extends BaseDonasiEntity {
-}
+public class TpaMasukEntity extends BaseDonasiEntity { }
