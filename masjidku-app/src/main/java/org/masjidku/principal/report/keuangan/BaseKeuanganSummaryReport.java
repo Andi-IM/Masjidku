2cf902c31a1b6abc8ede5c5546c076965af7f56b
Andi-IM <Andi.irhamm@users.noreply.github.com>
2026-07-02 11:27:28 +0700

    feat: add EditDonaturAnakYatim controller, BaseKeuanganSummaryReport, BaseAccountantController, and agent guidelines

--- masjidku-app/src/main/java/org/masjidku/principal/report/keuangan/BaseKeuanganSummaryReport.java
+++ masjidku-app/src/main/java/org/masjidku/principal/report/keuangan/BaseKeuanganSummaryReport.java
@@ -7,15 +7,8 @@ import org.masjidku.navigation.AppRouter;
 import org.masjidku.accounting.client.service.AccountingClient;
 import org.masjidku.util.ServiceProvider;
 
-public abstract class BaseKeuanganSummaryReport extends org.masjidku.controller.BaseAppController implements Initializable {
+public abstract class BaseKeuanganSummaryReport extends org.masjidku.accountant.BaseAccountantController {
     protected final AccountingClient client = ServiceProvider.get(AccountingClient.class);
 
-    @FXML public Text txtPemasukanTerakhir;
-    @FXML public Text txtPengeluaranTerakhir;
-    @FXML public Text txtTglPemasukkan;
-    @FXML public Text txtTotalPemasukkan;
-    @FXML public Text txtTotalPengeluaran;
-    @FXML public Text txtSaldo;
-    @FXML public Text txtTglPengeluaran;
     @FXML public void gotoHome() { mainApp.showData(); }
 }
